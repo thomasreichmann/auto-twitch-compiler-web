86895e26e6aa85b7aa55fb0c11208885bba78faf
thomasreichmann <thomasalmeidar@gmail.com>
2023-04-11 15:37:04 -0300

    divider save

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -2,32 +2,40 @@ import {
   AppBar,
   Box,
   Button,
+  Divider,
+  Grid,
   IconButton,
   Toolbar,
   Typography,
 } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
-import { signOut } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
 import React from "react";
 
-export const Header = () => (
-  <Box sx={{ flexGrow: 1 }}>
-    <AppBar position="static">
-      <Toolbar>
-        <IconButton
-          size="large"
-          edge="start"
-          color="inherit"
-          aria-label="menu"
-          sx={{ mr: 2 }}
-        >
-          <MenuIcon />
-        </IconButton>
-        <Typography sx={{ flexGrow: 1 }}>test</Typography>
-        <Button onClick={() => signOut()} variant="contained" color="warning">
-          Sign out
-        </Button>
-      </Toolbar>
-    </AppBar>
-  </Box>
-);
+export const Header = () => {
+  const { data: session, status } = useSession();
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6">{session?.user?.email}</Typography>
+          <Divider orientation="vertical" flexItem />
+          <Typography sx={{ flexGrow: 1 }}>{session?.user?.name}</Typography>
+          <Button onClick={() => signOut()} variant="contained" color="warning">
+            Sign out
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+};
