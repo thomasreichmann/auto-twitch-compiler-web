c6fa9d79b508018f999adcacb0947be8947a848c
thomasreichmann <thomasalmeidar@gmail.com>
2023-04-18 14:57:22 -0300

    base loading

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,8 @@ import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { NextPage } from "next";
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
-import { ReactElement, ReactNode } from "react";
+import { Router } from "next/router";
+import { ReactElement, ReactNode, useEffect, useState } from "react";
 
 const darkTheme = createTheme({
   palette: {
@@ -25,12 +26,40 @@ export default function App({
   pageProps: { session, ...pageProps },
 }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? Layout;
+  const loading = useLoading();
 
   return (
     <SessionProvider session={session}>
       <ThemeProvider theme={darkTheme}>
-        {getLayout(<Component {...pageProps} />)}
+        {getLayout(
+          !loading ? <Component {...pageProps} /> : <h1>loading...</h1>
+        )}
       </ThemeProvider>
     </SessionProvider>
   );
 }
+
+const useLoading = () => {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const start = () => {
+      console.log("start");
+      setLoading(true);
+    };
+    const end = () => {
+      console.log("finished");
+      setLoading(false);
+    };
+    Router.events.on("routeChangeStart", start);
+    Router.events.on("routeChangeComplete", end);
+    Router.events.on("routeChangeError", end);
+    return () => {
+      Router.events.off("routeChangeStart", start);
+      Router.events.off("routeChangeComplete", end);
+      Router.events.off("routeChangeError", end);
+    };
+  }, []);
+
+  return loading;
+};
