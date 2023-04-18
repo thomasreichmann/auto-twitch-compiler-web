e1d5dde1fcb8d2a3d2564dccc0c8d2b8a3d3fdde
thomasreichmann <thomasalmeidar@gmail.com>
2023-04-18 13:29:07 -0300

    added active likn

--- src/components/layout/drawerContent.tsx
+++ src/components/layout/drawerContent.tsx
@@ -5,9 +5,12 @@ import {
   ListItemText,
 } from "@mui/material";
 import { useRouter } from "next/router";
-import React, { ReactElement, ReactNode } from "react";
+import React, { ReactElement, useEffect, useState } from "react";
 import HomeIcon from "@mui/icons-material/Home";
 import InboxIcon from "@mui/icons-material/Inbox";
+
+const BASE_CLASS = "drawerLink";
+const ACTIVE_CLASS = "activeLink";
 
 type LinkProps = {
   icon: ReactElement;
@@ -17,13 +20,33 @@ type LinkProps = {
 
 const DrawerLink = (props: LinkProps) => {
   const router = useRouter();
+  const [className, setClassName] = useState(BASE_CLASS);
+
+  useEffect(() => {
+    // Entire code for computing active className might not be necessary at the end of the day.
+    if (!router.isReady) return;
+
+    const linkPathname = new URL(props.href as string, location.href).pathname;
+    const activePathname = new URL(router.asPath, location.href).pathname;
+
+    let computedClassName = "";
+
+    if (linkPathname == activePathname)
+      computedClassName = `${BASE_CLASS} ${ACTIVE_CLASS}`;
+    else computedClassName = BASE_CLASS;
+
+    setClassName(computedClassName);
+  }, [router.asPath, router.isReady, props.href]);
 
   const handleClick = () => {
     router.push(props.href);
   };
 
   return (
-    <ListItemButton onClick={handleClick}>
+    <ListItemButton
+      selected={className.match(ACTIVE_CLASS) != null}
+      onClick={handleClick}
+    >
       <ListItemIcon>{props.icon}</ListItemIcon>
       <ListItemText primary={props.title} />
     </ListItemButton>
@@ -35,8 +58,8 @@ const DrawerContent = () => {
     <>
       <List>
         <DrawerLink icon={<HomeIcon />} href="/" title="Home" />
-        <DrawerLink icon={<InboxIcon />} href="test" title="Test" />
-        <DrawerLink icon={<InboxIcon />} href="edit" title="Edit" />
+        <DrawerLink icon={<InboxIcon />} href="/test" title="Test" />
+        <DrawerLink icon={<InboxIcon />} href="/edit" title="Edit" />
       </List>
     </>
   );
