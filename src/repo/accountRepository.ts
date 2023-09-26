a23ecd56fd212bcdf5e7fbbdefebe47803e2cf48
thomasreichmann <thomasalmeidar@gmail.com>
2023-09-26 16:51:54 -0300

    fixed typing

--- src/repo/accountRepository.ts
+++ src/repo/accountRepository.ts
@@ -8,7 +8,7 @@ class AccountRepository {
   async findByUserId(userId: MongoId): Promise<Account | null> {
     let collection = await getCollection<Account>(ACCOUNTS);
 
-    let account = collection.findOne({ userId: new ObjectId(userId) });
+    let account = collection.findOne({ userId: new ObjectId(userId) as any });
 
     return account;
   }
@@ -16,10 +16,7 @@ class AccountRepository {
   async updateAccount(userId: MongoId, account: Account) {
     let collection = await getCollection<Account>(ACCOUNTS);
 
-    return collection.updateOne(
-      { userId: new ObjectId(userId) },
-      { $set: account }
-    );
+    return collection.updateOne({ userId: new ObjectId(userId) as any }, { $set: account });
   }
 }
 
--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,5 +1,4 @@
-import NextAuth from "next-auth";
-import { Account } from "next-auth";
+import "next-auth";
 
 declare module "next-auth" {
   interface Session {
