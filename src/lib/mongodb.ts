67402f6d47b1d4d88549139337838f8d6db2e0c6
thomasreichmann <thomasalmeidar@gmail.com>
2023-09-26 16:36:08 -0300

    upgraded modules

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,35 +1,13 @@
 import { MongoClient } from "mongodb";
 
-const uri = process.env.MONGODB_URI as string; // your mongodb connection string
-const options = {};
-
-declare global {
-  var _mongoClientPromise: Promise<MongoClient>;
+if (!process.env.MONGODB_URI) {
+  throw new Error('Invalid/Missing environment variable: "MONGODB_URI"');
 }
 
-class Singleton {
-  private static _instance: Singleton;
-  private client: MongoClient;
-  private clientPromise: Promise<MongoClient>;
-  private constructor() {
-    this.client = new MongoClient(uri, options);
-    this.clientPromise = this.client.connect();
-    if (process.env.NODE_ENV === "development") {
-      // In development mode, use a global variable so that the value
-      // is preserved across module reloads caused by HMR (Hot Module Replacement).
-      global._mongoClientPromise = this.clientPromise;
-    }
-  }
+const uri = process.env.MONGODB_URI;
 
-  public static get instance() {
-    if (!this._instance) {
-      this._instance = new Singleton();
-    }
-    return this._instance.clientPromise;
-  }
-}
-const clientPromise = Singleton.instance;
+// MongoClient now auto-connects so no need to store the connect()
+// promise anywhere and reference it.
+const client = new MongoClient(process.env.MONGODB_URI);
 
-// Export a module-scoped MongoClient promise. By doing this in a
-// separate module, the client can be shared across functions.
-export default clientPromise;
+export default client;
--- src/lib/repo.ts
+++ src/lib/repo.ts
@@ -1,12 +1,8 @@
 import { Collection, Document, ObjectId } from "mongodb";
-import clientPromise from "./mongodb";
+import client from "./mongodb";
 
 export type MongoId = string | ObjectId;
 
-export const getCollection = async <T extends Document>(
-  name: string
-): Promise<Collection<T>> => {
-  let client = await clientPromise;
-
+export const getCollection = async <T extends Document>(name: string): Promise<Collection<T>> => {
   return client.db().collection<T>(name);
 };
