d772709e2de510a5f967f346e10f5d0fbae275cc
Thomas Reichmann <thomasalmeidar@gmail.com>
2024-07-18 16:51:21 -0300

    .

--- src/pages/api/cron/games.ts
+++ src/pages/api/cron/games.ts
@@ -1,5 +1,4 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { fetch } from "next/dist/compiled/@edge-runtime/primitives";
 import csvParser from "csv-parser";
 import { PassThrough } from "stream";
 import { getCollection } from "@/lib/repo";
@@ -12,100 +11,117 @@ if (!process.env.TWITCH_CLIENT_SECRET) {
   throw new Error("Please add your twitch client secret to .env.local");
 }
 
-export const config = {
-  maxDuration: 60,
-};
-
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log("Getting token");
-  const urlWithQueryParams = new URL("https://id.twitch.tv/oauth2/token");
-  urlWithQueryParams.searchParams.append("client_id", process.env.TWITCH_CLIENT_ID!);
-  urlWithQueryParams.searchParams.append("client_secret", process.env.TWITCH_CLIENT_SECRET!);
-  urlWithQueryParams.searchParams.append("grant_type", "client_credentials");
+  try {
+    console.log("Getting token");
+    const urlWithQueryParams = new URL("https://id.twitch.tv/oauth2/token");
+    urlWithQueryParams.searchParams.append("client_id", process.env.TWITCH_CLIENT_ID!);
+    urlWithQueryParams.searchParams.append("client_secret", process.env.TWITCH_CLIENT_SECRET!);
+    urlWithQueryParams.searchParams.append("grant_type", "client_credentials");
 
-  let fRes = await fetch(urlWithQueryParams.toString(), {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
+    let fRes = await fetch(urlWithQueryParams.toString(), {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
 
-  if (!fRes.ok) {
-    throw new Error(`Failed to fetch token: ${fRes.statusText}`);
-  }
+    if (!fRes.ok) {
+      throw new Error(`Failed to fetch token: ${fRes.statusText}`);
+    }
 
-  let auth = await fRes.json();
-  let accessToken = auth.access_token;
+    let auth = await fRes.json();
+    let accessToken = auth.access_token;
 
-  console.log("Getting dump");
-  let dumpRes = await fetch("https://api.igdb.com/v4/dumps/games", {
-    method: "GET",
-    headers: {
-      "Client-ID": process.env.TWITCH_CLIENT_ID!,
-      Authorization: `Bearer ${accessToken}`,
-    },
-  });
+    console.log("Getting dump");
+    let dumpRes = await fetch("https://api.igdb.com/v4/dumps/games", {
+      method: "GET",
+      headers: {
+        "Client-ID": process.env.TWITCH_CLIENT_ID!,
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
 
-  if (!dumpRes.ok) {
-    throw new Error(`Failed to fetch dump: ${dumpRes.statusText}`);
-  }
+    if (!dumpRes.ok) {
+      throw new Error(`Failed to fetch dump: ${dumpRes.statusText}`);
+    }
 
-  let dr = await dumpRes.json();
-  let dumpUrl: string = dr.s3_url;
-  console.log(`Dump URL: ${dumpUrl}`);
+    let dr = await dumpRes.json();
+    let dumpUrl: string = dr.s3_url;
+    console.log(`Dump URL: ${dumpUrl}`);
 
-  console.log("Streaming CSV");
+    console.log("Streaming CSV");
 
-  const response = await fetch(dumpUrl);
-  if (!response.ok) {
-    throw new Error(`Failed to fetch CSV: ${response.statusText}`);
-  }
+    const response = await fetch(dumpUrl);
+    if (!response.ok) {
+      throw new Error(`Failed to fetch CSV: ${response.statusText}`);
+    }
 
-  const results: any[] = [];
-  const reader = response.body?.getReader();
-  const decoder = new TextDecoder();
+    const contentLength = response.headers.get("Content-Length");
+    const totalSize = contentLength ? parseInt(contentLength, 10) : null;
+    let loadedSize = 0;
 
-  async function processStream() {
-    let done = false;
-    while (!done) {
-      const { value, done: doneReading } = await reader!.read();
-      done = doneReading;
-      if (value) {
-        const chunk = decoder.decode(value, { stream: true });
-        const lines = chunk.split("\n");
-        for (const line of lines) {
-          // Push each line to the results array
-          results.push(line);
+    const results: any[] = [];
+    const reader = response.body!.getReader();
+    const decoder = new TextDecoder();
+
+    async function processStream() {
+      let done = false;
+      while (!done) {
+        const { value, done: doneReading } = await reader.read();
+        done = doneReading;
+        if (value) {
+          loadedSize += value.length;
+          const chunk = decoder.decode(value, { stream: true });
+          const lines = chunk.split("\n");
+          for (const line of lines) {
+            // Push each line to the results array
+            results.push(line);
+          }
+          if (totalSize) {
+            console.log(`Progress: ${((loadedSize / totalSize) * 100).toFixed(2)}%`);
+          } else {
+            console.log(`Loaded ${loadedSize} bytes`);
+          }
         }
       }
     }
-  }
 
-  await processStream();
+    await processStream();
 
-  // Convert CSV lines to JSON using csv-parser
-  const stream = new PassThrough();
-  stream.end(results.join("\n"));
-  const jsonResults: any[] = [];
+    // Convert CSV lines to JSON using csv-parser
+    const stream = new PassThrough();
+    stream.end(results.join("\n"));
+    const jsonResults: any[] = [];
 
-  stream
-    .pipe(csvParser())
-    .on("data", (data) => jsonResults.push(data))
-    .on("end", async () => {
-      // Get the collection and perform the update
-      let collection = await getCollection<AvailableGame>("games");
+    return new Promise((resolve, reject) => {
+      stream
+        .pipe(csvParser())
+        .on("data", (data) => jsonResults.push(data))
+        .on("end", async () => {
+          console.log("CSV parsing completed");
 
-      // Clear the existing collection
-      await collection.deleteMany({});
+          // Get the collection and perform the update
+          let collection = await getCollection<AvailableGame>("games");
 
-      // Insert new items
-      await collection.insertMany(jsonResults);
+          // Clear the existing collection
+          console.log("Deleting current collection games");
+          await collection.deleteMany({});
 
-      console.log("Collection updated successfully");
-      res.status(200).json(jsonResults.length);
-    })
-    .on("error", (error) => {
-      console.error("Error during CSV parsing", error);
-      res.status(500).json({ error: error.message });
+          // Insert new items
+          console.log("Adding new games to collection");
+          await collection.insertMany(jsonResults);
+
+          console.log("Collection updated successfully");
+          resolve(res.status(200).json(jsonResults));
+        })
+        .on("error", (error) => {
+          console.error("Error during CSV parsing", error);
+          reject(res.status(500).json({ error: error.message }));
+        });
     });
+  } catch (error: any) {
+    console.error(error);
+    res.status(500).json({ error: error.message });
+  }
 }
